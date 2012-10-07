d53a55396d0fc688cb176f4f2840efef97409c67
alvi0010 <alvintamie@gmail.com>
2012-10-08 03:53:00 +0800

    init 1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoodrecoResultsServer::Application.config.secret_token = '89f6fea6ead5fb72fb46d565586d068822a7e3799ac66367d9d312fbf41949c7c5aa818abeecaed795807b028cd77066844b6f8ec533381e6779baa3cb9caa51'
